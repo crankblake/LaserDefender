8f85c7cd9dd0c9d1c4751868a7bedcf3ed0ba663
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-01-21 17:45:08 -0700

    SpawnAllWaves!

--- LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
-    [SerializeField] int startingWave = 0;
+    [SerializeField] readonly int startingWave = 0;
+    [SerializeField] bool looping = false;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
-        StartCoroutine(SpawnAllWaves());
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
     }
 
     private IEnumerator SpawnAllWaves()
